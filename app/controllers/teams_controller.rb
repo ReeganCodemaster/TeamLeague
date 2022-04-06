3251989a1c72078f1b37d3f9629a6c0e2d06281b
ReeganCodemaster <reeganviljoen@outlook.com>
2022-04-06 22:34:11 +0200

    created new , create methodas in teams controller

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -6,4 +6,24 @@ class TeamsController < ApplicationController
     def show
         @team = Team.find(params[:id])
     end
+
+    def new
+        @team = Team.new
+    end
+
+    def create
+        @team = Team.new(teams_params)
+        
+        if @team.save
+            redirect_to @team
+        else
+            render :new , status: :unprocessable_entity
+        end
+    end
+
+    private
+
+    def teams_params
+        params.require(:teams).permit(:title)
+    end
 end
