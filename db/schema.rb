bf421fecd96a56a3fe9aa41890e28106fb09f302
ReeganCodemaster <reeganviljoen@outlook.com>
2022-04-06 17:19:04 +0200

    migrated db

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_06_151634) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_06_151826) do
+  create_table "players", force: :cascade do |t|
+    t.string "player_name"
+    t.integer "team_id", null: false
+    t.boolean "coordinator"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["team_id"], name: "index_players_on_team_id"
+  end
+
   create_table "teams", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "players", "teams"
 end
