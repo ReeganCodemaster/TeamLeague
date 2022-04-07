fb2644dbddfd6923a0c43e9a2660eedea4ed39b2
ReeganCodemaster <reeganviljoen@outlook.com>
2022-04-07 12:45:47 +0200

    added players table

--- app/models/game.rb
+++
@@ -1,4 +0,0 @@
-class Game < ApplicationRecord
-  belongs_to :team_1
-  belongs_to :team_2
-end
--- db/migrate/20220407104453_create_players.rb
+++ db/migrate/20220407104453_create_players.rb
@@ -3,7 +3,6 @@ class CreatePlayers < ActiveRecord::Migration[7.0]
     create_table :players do |t|
       t.string :player_name
       t.references :team, null: false, foreign_key: true
-      t.boolean :coordinator
 
       t.timestamps
     end
