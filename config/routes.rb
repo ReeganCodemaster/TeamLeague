0cbd150186c0a32c47acfd43a957bee3e5a925c1
ReeganCodemaster <reeganviljoen@outlook.com>
2022-04-07 10:20:34 +0200

    added reourcfull routing to players

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root "teams#index"
   resources :teams do 
-    :players
+    resources :players
   end
   # Defines the root path route ("/")
   # root "articles#index"
