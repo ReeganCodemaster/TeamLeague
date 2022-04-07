2289c8fa713fa0feb9b5e843d767c742d6586bf5
ReeganCodemaster <reeganviljoen@outlook.com>
2022-04-07 09:20:29 +0200

    added nested resourcfull routigng for players

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root "teams#index"
-  resources :teams
+  resources :teams do 
+    :players
   # Defines the root path route ("/")
   # root "articles#index"
 end
