3f21db64dbc63b421aeae26f071e422dc213db50
ReeganCodemaster <reeganviljoen@outlook.com>
2022-04-15 14:30:33 +0200

    added strong params & before actions for game & team

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,2 +1,17 @@
 class GamesController < ApplicationController
+  before_action :set_game, only: %i[show edit update destroy delete]
+  before_action :set_team
+
+  private
+  def set_team
+    @team = Team.find(params[:team_id])
+  end
+
+  def set_game
+    @game = @team.games.find(params[:id])
+  end
+
+  def games_params
+    params.require(:games).permit
+  end
 end
