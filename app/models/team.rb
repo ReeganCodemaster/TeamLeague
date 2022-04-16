8380be07e5e2235acd9442988b016268cf989a3e
ReeganCodemaster <reeganviljoen@outlook.com>
2022-04-16 11:42:07 +0200

    specified custom foregn_key for games

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,4 +1,4 @@
 class Team < ApplicationRecord
     has_many :players, dependent: :destroy
-    has_many :games, dependent: :destroy
+    has_many :games, foreign_key: 'team_1_id', foreign_key: 'team_2_id', dependent: :destroy
 end
