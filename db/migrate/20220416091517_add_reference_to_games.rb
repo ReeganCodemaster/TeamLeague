d7d2b8d8cf72b84aebb99752a4aa5aabd4c45718
ReeganCodemaster <reeganviljoen@outlook.com>
2022-04-16 11:42:26 +0200

    removed unnescary indexes

--- db/migrate/20220416091517_add_reference_to_games.rb
+++ db/migrate/20220416091517_add_reference_to_games.rb
@@ -1,6 +1,6 @@
 class AddReferenceToGames < ActiveRecord::Migration[7.0]
   def change
-    add_reference :games, :team_1, refernces: :teams, index: true
-    add_reference :games, :team_2, refernces: :teams, index: true
+    add_reference :games, :team_1, refernces: :teams
+    add_reference :games, :team_2, refernces: :teams
   end
 end
