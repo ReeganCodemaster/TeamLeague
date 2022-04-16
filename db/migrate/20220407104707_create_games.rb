7194f548c1ccb94890362cc5eba910aa80b622ad
ReeganCodemaster <reeganviljoen@outlook.com>
2022-04-16 11:23:17 +0200

    removed buggy refrences

--- db/migrate/20220407104707_create_games.rb
+++ db/migrate/20220407104707_create_games.rb
@@ -1,8 +1,8 @@
 class CreateGames < ActiveRecord::Migration[7.0]
   def change
     create_table :games do |t|
-      t.references :team_1, null: false, foreign_key: true
-      t.references :team_2, null: false, foreign_key: true
+      # t.references :team_1, null: false, foreign_key: true
+      # t.references :team_2, null: false, foreign_key: true
       t.integer :team_1_score
       t.integer :team_2_score
 
