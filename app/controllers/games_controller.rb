c2530985c2273fc9d755fb839d9c5f05de1d3c43
ReeganCodemaster <reeganviljoen@outlook.com>
2022-04-20 17:20:08 +0200

    added index method to list all teams and there pojnts

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,6 +2,21 @@ class GamesController < ApplicationController
   before_action :set_game, only: %i[show edit update destroy]
   before_action :set_team
 
+  def index
+    @games = Game.all
+    @points = {}
+    @games.each do |game|
+      service_hash = PointsService.call(game.team_1_id, game.team_2_id)  
+      if @points[game.team_1_id].empty? and @points[game.team_2_id].empty?
+        points >> service_hash
+      elsif @points[game.team_1_id].empty? and !@points[game.team_2_id].empty?
+        points[game.team_1_id] = service_hash[game.team_1_id]
+      elsif !@points[game.team_1_id].empty? and @points[game.team_2_id].empty?
+        points[game.team_2_id] = service_hash[game.team_2_id]
+    end 
+    
+  end
+
   def new
     @game = Game.new()
   end
