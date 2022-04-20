17d1d157769779ed6ebcf96c1b598591c63d52f4
ReeganCodemaster <reeganviljoen@outlook.com>
2022-04-20 14:22:59 +0200

    added tournaments route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root "teams#index"
   resources :teams do 
-    get :tournament on: :member
+    get :tournament, on: :collection
     resources :players do
       get :delete, on: :member
     end
