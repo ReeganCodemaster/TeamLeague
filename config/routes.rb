5accc49bd14d7d2c8a20076b7ebc5c012194fe2c
ReeganCodemaster <reeganviljoen@outlook.com>
2022-04-20 11:13:45 +0200

    added tournamt route

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root "teams#index"
   resources :teams do 
+    get :tournament on: :member
     resources :players do
       get :delete, on: :member
     end
     resources :games
   end
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
