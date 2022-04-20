f2f74c774956bdb40423bb17e9120dc30002015b
ReeganCodemaster <reeganviljoen@outlook.com>
2022-04-20 16:16:31 +0200

    added bfore save callback for points

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,4 +1,20 @@
 class Game < ApplicationRecord
   belongs_to :team_1, class_name: "Team", foreign_key: "team_1_id"
   belongs_to :team_2, class_name: "Team", foreign_key: "team_2_id"
+
+  before_save :generate_points
+
+  private
+  def generate_points
+    if team_1_score > team_2_score
+      self.team_1_points = 3
+      self.team_2_points = 0
+    elsif team_1_score = team_2_score
+      self.team_1_points = 1
+      self.team_2_points = 1
+    else
+      self.team_2_points = 3
+      self.team_1_points = 0
+    end 
+  end
 end
