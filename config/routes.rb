ce61ce358a415097ee1cea976641aa4b189cbf9a
ReeganCodemaster <reeganviljoen@outlook.com>
2022-04-21 07:13:47 +0200

    fixed games routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   root "teams#index"
   resources :teams do 
-    get :tournament, on: :collection
     resources :players do
       get :delete, on: :member
     end
-    resources :games
+  end
+  resources :games do
+    get :tournament, on: :collection
   end
 end
