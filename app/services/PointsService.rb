79c77a6f42d459d2e5468fea37cb3d9ae2ef510d
ReeganCodemaster <reeganviljoen@outlook.com>
2022-04-21 06:48:16 +0200

    changed how hash was declared

--- app/services/PointsService.rb
+++ app/services/PointsService.rb
@@ -6,7 +6,7 @@ class PointsService
   end
 
   def call
-    total_points = {}
+    total_points = Hash.new
     total_points[@team_1_id] = Game.where(team_1_id: @team_1_id).sum(:team_1_points)
     total_points[@team_2_id] = Game.where(team_2_id: @team_2_id).sum(:team_2_points)
     total_points
