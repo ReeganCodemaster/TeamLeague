bb0d6d2b168752ef21c69655575409e3e9a395b0
ReeganCodemaster <reeganviljoen@outlook.com>
2022-04-23 09:12:19 +0200

    refactored players controller

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -3,53 +3,53 @@ class PlayersController < ApplicationController
     before_action :set_team
 
     def new  
-        @team = Team.find(params[:team_id])
-        @player = @team.players.new()
-        @coordinator = @team.password == params[:password] ? true : false
+      @team = Team.find(params[:team_id])
+      @player = @team.players.new()
+      @coordinator = @team.password == params[:password] ? true : false
     end
 
     def create
-        @team = Team.find(params[:team_id])
-        @player = @team.players.new(player_params)
+      @team = Team.find(params[:team_id])
+      @player = @team.players.new(player_params)
 
-        if @player.save
-            redirect_to @team
-        else
-            render :new, status: :unprocessable_entity
-        end
+      if @player.save
+        redirect_to @team
+      else
+        render :new, status: :unprocessable_entity
+      end
     end
+
     def edit
-        @coordinator = @team.password == params[:password] ? true : false
+      @coordinator = @team.password == params[:password] ? true : false
     end
+
     def update
-        if @player.update(player_params)
-            redirect_to @team
-        else
-            render :edit , status: :unprocessable_entity
-        end
+      if @player.update(player_params)
+        redirect_to @team
+      else
+        render :edit , status: :unprocessable_entity
+      end
     end
 
     def delete
-        @coordinator = @team.password == params[:password] ? true : false
+      @coordinator = @team.password == params[:password] ? true : false
     end
     
     def destroy 
-        @player.destroy
-
-        redirect_to @team, status: 303
+      @player.destroy
+      redirect_to @team, status: 303
     end
 
     private
-
     def set_team 
-        @team = Team.find(params[:team_id])
+      @team = Team.find(params[:team_id])
     end
 
     def set_player 
-        @player = Player.find(params[:id])
+      @player = Player.find(params[:id])
     end
 
     def player_params
-        params.require(:player).permit(:player_name)
+      params.require(:player).permit(:player_name)
     end
 end
