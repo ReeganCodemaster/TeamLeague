b28eecaf8a3e402fdd15c7744079cc234c464b66
ReeganCodemaster <reeganviljoen@outlook.com>
2022-04-23 09:17:44 +0200

    refactored teams controller

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,52 +1,50 @@
 class TeamsController < ApplicationController
     
-    def index
-        @teams = Team.all
+  def index
+    @teams = Team.all
+  end
+
+  def show
+    @team = Team.find(params[:id])
+    @games = Game.where(team_1_id:params[:id]).or(Game.where(team_2_id:params[:id]))
+  end
+
+  def new
+    @team = Team.new
+  end
+
+  def create
+    @team = Team.new(teams_params)
+      
+    if @team.save
+      redirect_to @team
+    else
+      render :new , status: :unprocessable_entity
     end
+  end
 
-    def show
-        @team = Team.find(params[:id])
-        @games = Game.where(team_1_id:params[:id]).or(Game.where(team_2_id:params[:id]))
+  def edit
+    @team = Team.find(params[:id])
+  end
+
+  def update
+    @team = Team.find(params[:id])
+
+    if @team.update(teams_params)
+      redirect_to @team
+    else
+      render :edit, status: :unprocessable_entity
     end
+  end
 
-    def new
-        @team = Team.new
-    end
+  def destroy
+    @team = Team.find(params[:id])
+    @team.destroy
+    redirect_to root_path, status: 303
+  end
 
-    def create
-        @team = Team.new(teams_params)
-        
-        if @team.save
-            redirect_to @team
-        else
-            render :new , status: :unprocessable_entity
-        end
-    end
-
-    def edit
-        @team = Team.find(params[:id])
-    end
-
-    def update
-        @team = Team.find(params[:id])
-
-        if @team.update(teams_params)
-            redirect_to @team
-        else
-            render :edit, status: :unprocessable_entity
-        end
-    end
-
-    def destroy
-        @team = Team.find(params[:id])
-        @team.destroy
-
-        redirect_to root_path, status: 303
-    end
-
-    private
-
-    def teams_params
-        params.require(:team).permit(:title, :coordinator, :password)
-    end
+  private
+  def teams_params
+    params.require(:team).permit(:title, :coordinator, :password)
+  end
 end
