feb4aff5b9e124709cae7125db351a80c82b777d
ReeganCodemaster <reeganviljoen@outlook.com>
2022-04-23 09:36:58 +0200

    refactored router

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   root "teams#index"
+  
   resources :teams do 
     resources :players do
       get :delete, on: :member
     end
   end
+
   resources :games do
     get :tournament, on: :collection
   end
