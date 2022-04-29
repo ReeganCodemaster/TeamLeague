26cd0a01bfd5a20f5c764b5022ff26ed5b4e042d
ReeganCodemaster <reeganviljoen@outlook.com>
2022-04-29 15:55:18 +0200

    refactored points service

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,24 +2,8 @@ class GamesController < ApplicationController
   before_action :set_game, only: %i[show edit update destroy]
 
   def index
-    @games = Game.all
-    @points = Hash.new()
-
-    @games.each do |game|
-      point_service = PointsService.new(game.team_1_id, game.team_2_id) 
-      points_hash = point_service.call()
-
-      if points_hash[game.team_1_id] > 0 and points_hash[game.team_2_id] > 0
-        @points.merge!(points_hash) 
-      elsif points_hash[game.team_1_id] > 0 and points_hash[game.team_2_id] = 0
-        @points[game.team_1_id] = points_hash[game.team_1_id]
-        @points[game.team_2_id] = @points[game.team_2_id].to_i + points_hash[game.team_2_id]
-      elsif points_hash[game.team_1_id] = 0 and points_hash[game.team_2_id] > 0
-        @points[game.team_2_id] = points_hash[game.team_2_id]
-        @points[game.team_1_id] = @points[game.team_1_id].to_i + points_hash[game.team_1_id]
-      end
-    end 
-    @points = @points.sort_by{|key, value| value}.reverse
+    point_service = PointsService.new()
+    @points = point_service.call()
   end
 
   def new
@@ -54,19 +38,24 @@ class GamesController < ApplicationController
   end
 
   def tournament
+
+    point_service = PointsService.new() 
+
     @match_ups =  {} 
-    @team_num = params[:team_num]   
-    k = 0
-    @points.each do |team|
-      k += 1  
-      
-      if k <= @team_num
-        if k % 2 == 0
-          match_ups[prev_team] = team[0]
-        else
-          prev_team = team[0]
+    @team_num = params[:team_num] 
+    if @team_num != nil  
+      k = 0
+      prev_team = ''
+      @points.each do |team|
+        k += 1  
+        if k <= @team_num.to_i
+          if k % 2 == 0
+            @match_ups[prev_team] = team[0]
+          else
+            prev_team = team[0]
+          end
         end
-      end
+      end 
     end
   end
 
--- app/services/PointsService.rb
+++ app/services/PointsService.rb
@@ -1,25 +1,32 @@
 class PointsService
   
-  def initialize(games)
-    @games = games
+  def initialize()
+    @games = Game.all
   end
 
   def call()
     total_points = Hash.new
-
+    points = Hash.new
     @games.each do |game|
-      total_points[game.team_1_id] = Game.where(team_1_id: game.team_1_id).sum(:team_1_points)
-      total_points[game.team_2_id] = Game.where(team_2_id: game.team_2_id).sum(:team_2_points)
 
-      if total_points[game.team_1_id] > 0 and total_points[game.team_2_id] > 0
-        total_points.merge!(total_points) 
-      elsif total_points[game.team_1_id] > 0 and total_points[game.team_2_id] = 0
-        total_points[game.team_1_id] = total_points[game.team_1_id]
-        total_points[game.team_2_id] = total_points[game.team_2_id].to_i + total_points[game.team_2_id]
-      elsif total_points[game.team_1_id] = 0 and total_points[game.team_2_id] > 0
-        total_points[game.team_2_id] = total_points[game.team_2_id]
-        total_points[game.team_1_id] = total_points[game.team_1_id].to_i + total_points[game.team_1_id]
+
+      points[game.team_1_id] = Game.where(team_1_id: game.team_1_id).find(game.id).team_1_points
+      points[game.team_2_id] = Game.where(team_2_id: game.team_2_id).find(game.id).team_2_points
+
+      if total_points[game.team_1_id].to_i > 0 and total_points[game.team_2_id].to_i > 0 
+        total_points[game.team_1_id] += points[game.team_1_id]
+        total_points[game.team_2_id] += points[game.team_2_id]
+      elsif total_points[game.team_1_id].to_i > 0 and total_points[game.team_2_id].to_i == 0
+        total_points[game.team_1_id] += points[game.team_1_id]
+        total_points[game.team_2_id] = points[game.team_2_id]
+      elsif total_points[game.team_1_id].to_i == 0 and total_points[game.team_2_id].to_i > 0
+        total_points[game.team_2_id] += points[game.team_2_id]
+        total_points[game.team_1_id] = points[game.team_1_id]
+      else 
+        total_points[game.team_2_id] = points[game.team_2_id]
+        total_points[game.team_1_id] = points[game.team_1_id]
       end
+      points.clear
     end 
     total_points = total_points.sort_by{|key, value| value}.reverse 
   end
