4bffbd63ac736a772bc47d9552012296f753e72b
ReeganCodemaster <reeganviljoen@outlook.com>
2022-04-30 21:09:01 +0200

    added has_many/belongs to association

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,6 +1,7 @@
 class Game < ApplicationRecord
   belongs_to :team_1,class_name: 'Team', foreign_key: 'team_1_id'
   belongs_to :team_2,class_name: 'Team', foreign_key: 'team_2_id'
+  belongs_to :tournament
 
   before_save :generate_points
 
