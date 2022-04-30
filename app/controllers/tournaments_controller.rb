4aed23cae66df8cddcc2fd1dd2b92edbda1e82f7
ReeganCodemaster <reeganviljoen@outlook.com>
2022-04-30 21:43:05 +0200

    added new cretae methods to tournaments controller

--- app/controllers/tournaments_controller.rb
+++ app/controllers/tournaments_controller.rb
@@ -5,9 +5,26 @@ class TournamentsController < ApplicationController
     @tournaments = Tournament.all
   end
   
+  def new
+    @tournament = Tournament.new()
+  end
+
+  def create
+    @tournament = Tournament.new(tournament_params)
+    
+    if @tournament.save
+      redirect_to @tournament
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+  
   private
   def set_tournament  
     @tournament = Tournament.find(params[:id])
   end
-
+  
+  def tournament_params
+    params.require(:tournament).permit(:title, :team_num)
+  end
 end
