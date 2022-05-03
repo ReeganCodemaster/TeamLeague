b164804d684f0292b9fcc6abe26fc4ad4fc09203
ReeganCodemaster <reeganviljoen@outlook.com>
2022-05-03 15:20:31 +0200

    refactored PointsService

--- app/services/PointsService.rb
+++ app/services/PointsService.rb
@@ -1,33 +1,16 @@
 class PointsService
   
   def initialize()
-    @games = Game.all
+    @teams = Team.all
   end
 
   def call()
     total_points = Hash.new
-    points = Hash.new
-    @games.each do |game|
-
-
-      points[game.team_1_id] = Game.where(team_1_id: game.team_1_id).find(game.id).team_1_points
-      points[game.team_2_id] = Game.where(team_2_id: game.team_2_id).find(game.id).team_2_points
-
-      if total_points[game.team_1_id].to_i > 0 and total_points[game.team_2_id].to_i > 0 
-        total_points[game.team_1_id] += points[game.team_1_id]
-        total_points[game.team_2_id] += points[game.team_2_id]
-      elsif total_points[game.team_1_id].to_i > 0 and total_points[game.team_2_id].to_i == 0
-        total_points[game.team_1_id] += points[game.team_1_id]
-        total_points[game.team_2_id] = points[game.team_2_id]
-      elsif total_points[game.team_1_id].to_i == 0 and total_points[game.team_2_id].to_i > 0
-        total_points[game.team_2_id] += points[game.team_2_id]
-        total_points[game.team_1_id] = points[game.team_1_id]
-      else 
-        total_points[game.team_2_id] = points[game.team_2_id]
-        total_points[game.team_1_id] = points[game.team_1_id]
-      end
-      points.clear
-    end 
+    @teams.each do |team|
+      team_1_points = Game.where(team_1_id: team.id).sum(:team_1_points)
+      team_2_points = Game.where(team_2_id: team.id).sum(:team_2_points)
+      total_points[team.id] = team_1_points + team_2_points
+    end
     total_points = total_points.sort_by{|key, value| value}.reverse 
   end
   
