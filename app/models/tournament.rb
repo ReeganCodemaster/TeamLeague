8c15e78bd1c2dbf7f64819908c807a6ee66e3f40
ReeganCodemaster <reeganviljoen@outlook.com>
2022-05-04 09:09:43 +0200

    added prence validations

--- app/models/tournament.rb
+++ app/models/tournament.rb
@@ -1,5 +1,6 @@
 class Tournament < ApplicationRecord
   has_many :games, dependent: :destroy
 
-  validates :team_num, numericality: {greater_than_or_equal_to: 0, allow_nil:false, even:true}
+  validates :team_num, numericality: {greater_than_or_equal_to: 0, allow_nil:false, even:true}, presence:true
+  validates :title, presence:true
 end
