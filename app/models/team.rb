c4b7fa1a34401cf76559c8761a5127985968f37a
ReeganCodemaster <reeganviljoen@outlook.com>
2022-05-04 09:08:23 +0200

    added prence validations to team model

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,4 +1,8 @@
 class Team < ApplicationRecord
-    has_many :players, dependent: :destroy
-    has_many :games
+  has_many :players, dependent: :destroy
+  has_many :games
+
+  validates :title, presence:true
+  validates :coordinator, presence:true
+  validates :password, presence:true
 end
